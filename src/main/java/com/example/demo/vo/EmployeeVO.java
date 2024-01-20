e18ae7398b073e18cc1a5f0aec774b958f268900
AulaGitTR <aulagittr@gmail.com>
2024-01-20 11:59:53 -0300

    Add Swagger Upgrade Teste de integracao.

--- src/main/java/com/example/demo/vo/EmployeeVO.java
+++ src/main/java/com/example/demo/vo/EmployeeVO.java
@@ -1,13 +1,14 @@
 package com.example.demo.vo;
 
 public class EmployeeVO {
-	
+
 	private Long id;
 	private String name;
 	private String role;
-	
-	public EmployeeVO() {}
-	
+
+	public EmployeeVO() {
+	}
+
 	public EmployeeVO(String name, String role) {
 		this.name = name;
 		this.role = role;
@@ -16,18 +17,23 @@ public class EmployeeVO {
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
--- src/test/java/com/example/demo/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/demo/controller/EmployeeControllerTest.java
@@ -16,7 +16,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,9 +31,13 @@ import com.example.demo.repository.EmployeeRepository;
 //@RunWith(MockitoJUnitRunner.class)
 public class EmployeeControllerTest {
 
+	//Mock: cria uma instancia de uma classe, porém Mockada. 
+	//Se você chamar um metodo ele não irá chamar o metodo real, 
+	//a não ser que você queira.
 	@Mock
 	private EmployeeRepository employeeRepository;
-	
+
+	//InjectMocks: criar uma intancia e injeta as dependências necessárias que estão anotadas com @Mock.
 	@InjectMocks
 	private EmployeeController employeeController;
 
@@ -42,23 +48,34 @@ public class EmployeeControllerTest {
 
 	@Test
 	public void all_test() {
-		 when(employeeRepository.findAll()).thenReturn(createEmployeeMock());
-		 assertNotNull(employeeController.all());
+		 List<Employee> inputemployees = createEmployeeMock();
+		 when(employeeRepository.findAll()).thenReturn(inputemployees);
+		 ResponseEntity<CollectionModel<EntityModel<Employee>>> employees = employeeController.all();
+		 assertNotNull(employees);
+		 assertEquals(HttpStatus.OK, employees.getStatusCode());
+		 assertEquals(inputemployees.size(), employees.getBody().getContent().size());
+		 int i = 0;
+		 for (EntityModel<Employee> employee : employees.getBody().getContent()) {
+			 assertEquals(inputemployees.get(i), employee.getContent());
+			 ++i;
+		}
 	}
 
 	@Test
-	public void one_ok () {
-		when(employeeRepository.findById(anyLong())).thenReturn(Optional.ofNullable(EmployeeTest.create()));
-		ResponseEntity<EntityModel<Employee>> employee = employeeController.one(1L);
-		assertEquals("Bilbo Baggins", employee.getBody().getContent().getName());
-		assertEquals("burglar", employee.getBody().getContent().getRole());
+	public void one_ok() {
+		Employee employeeInput = EmployeeTest.create();
+		//Matchers: permite a verificação por meio de matchers de argumentos (anyObject(), anyString() …)
+		when(employeeRepository.findById(anyLong())).thenReturn(Optional.ofNullable(employeeInput));
+		ResponseEntity<EntityModel<Employee>> employeeOutput = employeeController.one(1L);
+		assertEquals(employeeInput.getName(), employeeOutput.getBody().getContent().getName());
+		assertEquals(employeeInput.getRole(), employeeOutput.getBody().getContent().getRole());
 	}
-	
+
 	@Test(expected = EmployeeNotFoundException.class)
-	public void one_NotOk () {
+	public void one_NotOk() {
 		employeeController.one(2L);
 	}
-	
+
 	@Test
 	public void newEmployee_test() {
 		Employee employee = EmployeeTest.create();
@@ -67,46 +84,74 @@ public class EmployeeControllerTest {
 		assertEquals(employee.getName(), newEmployee.getBody().getName());
 		assertEquals(employee.getRole(), newEmployee.getBody().getRole());
 	}
-	
+
 	@Test
 	public void replaceEmployee() {
 		Employee employee = EmployeeTest.create();
+		//When: Após um mock ser criado, você pode configurar ações na chamada e o retorno.
 		when(employeeRepository.findById(1L)).thenReturn(Optional.of(employee));
 		when(employeeRepository.save(employee)).thenReturn(employee);
 		employee.setName("altName");
 		ResponseEntity<Employee> employeereplace = employeeController.replaceEmployee(employee, 1L);
 		assertEquals("altName", employeereplace.getBody().getName());
 	}
-	
+
 	@Test()
 	public void replaceEmployeeNotFound() {
 		Employee employee = new Employee();
 		assertEquals(HttpStatus.NO_CONTENT, employeeController.replaceEmployee(employee, 5L).getStatusCode());
 	}
-	
+
+	/*
+	 * Testando um metodo void
+	 * Primeiro chamamos o método que vai ser testado e passamos os
+	 * parâmetros necessários.
+	 * Em seguida usamos o Mockito.verify para verificar se durante a execução das classes mocadas foi chamado o método em questão. 
+	 * Podemos verificar varias coisas com Mockito.verify numero de vezes que executou, parâmetros recebidos e etc.
+	 */
 	@Test
 	public void deleteEmployee() {
 		employeeController.deleteEmployee(1L);
+		//Verify: verifica a quantidade de vezes e quais parametros 
+		//utilizados para acessar um determinado metodo.
 		Mockito.verify(employeeRepository, Mockito.times(1)).deleteById(1L);
 	}
 	
+	@Spy
+	List<String> list = new ArrayList<String>();
+
 	@Test
 	public void spyTest() {
-		List<Employee> employees = Mockito.spy(new ArrayList<Employee>());
-		Employee one = new Employee("Bilbo Baggins", "burglar");
-		Employee two = new Employee("Frodo Baggins", "thief");
+		//Spy: cria uma instancia de uma classe, que você pode mockar ou chamar 
+		//os metodos reais. 
+		//É uma alternativa ao InjectMocks, quando é preciso mockar 
+		//metodos da propria classe que esta sendo testada.
+//		List<Employee> employees = Mockito.spy(new ArrayList<Employee>());
+//		Employee one = new Employee("Bilbo Baggins", "burglar");
+//		Employee two = new Employee("Frodo Baggins", "thief");
+//
+//		employees.add(one);
+//		employees.add(two);
+//
+//		Mockito.verify(employees).add(one);
+//		Mockito.verify(employees).add(two);
+//		assertEquals(2, employees.size());
+//
+//		Mockito.doReturn(100).when(employees).size();
+//		assertEquals(100, employees.size());
 		
-		employees.add(one);
-		employees.add(two);
 		
-		Mockito.verify(employees).add(one);
-	    Mockito.verify(employees).add(two);
-	    assertEquals(2, employees.size());
-
-	    Mockito.doReturn(100).when(employees).size();
-	    assertEquals(100, employees.size());
+//	    List<String> spyList = Mockito.spy(list);
+//
+//	    spyList.add("one");
+//	    spyList.add("two");
+//
+//	    Mockito.verify(spyList).add("one");
+//	    Mockito.verify(spyList).add("two");
+//
+//	    assertEquals(2, spyList.size());
 	}
-	
+
 	private List<Employee> createEmployeeMock() {
 		List<Employee> all = new ArrayList<Employee>();
 		all.add(new Employee("Bilbo Baggins", "burglar"));
